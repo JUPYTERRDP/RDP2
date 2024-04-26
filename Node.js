fc266baa310350219c790c6ecff4d0e52a5a650b
JUPYTERRDP <167531064+JUPYTERRDP@users.noreply.github.com>
2024-04-26 18:41:56 +0530

    Update and rename access-control.yml to Node.js

---
+++ Node.js
@@ -0,0 +1,11 @@
+{
+  "name": "my-app",
+  "version": "1.0.0",
+  "description": "My Chrome Remote Desktop application",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1"
+  }
+}
